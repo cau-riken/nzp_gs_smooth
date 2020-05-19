b5a263ff45faa997fb61543c3a36512e3fc9955c
Alexander Woodward <alexander.woodward@riken.jp>
2020-05-19 11:48:04 +0900

    Refactoring of the main.py code.

--- main.py
+++ main.py
@@ -18,10 +18,11 @@ from datetime import datetime
 import argparse
 import nzp_gs_smooth
 
-def main(inputFolder): 
+def create_dirs(): 
     print("Creating required folders.")
     print("This removes existing folders and data. Be careful!")
     cwd = os.getcwd()
+    
     working_dir = cwd + "/working/"
     out_images = cwd+'/out_images'
     out_transforms = cwd+'/out_transforms'
@@ -42,18 +43,29 @@ def main(inputFolder):
     os.mkdir(working_dir+'/i_hat_image')#OutputIHAT
     os.mkdir(working_dir+'/registration_output_image')#Output
 
-    # multiple of 2 is best for 'iterations'
-    nzp_gs_smooth.run(inputFolder, working_dir, out_images, out_transforms, 2, 20)
+    dirs = {
+        'working_dir': working_dir,
+        'out_images': out_images,
+        'out_transforms': out_transforms
+    }
+
+    return dirs
+    
 
 if __name__ == '__main__':
     now = datetime.now()
     current_time_start = now.strftime("%H:%M:%S")
     print("Current time = ", current_time_start)
     parser = argparse.ArgumentParser(description='Standalone version of gauss-seidel smoothing of image stack.')
-    parser.add_argument('inputFolder', metavar='inputFolder',
-                    help='a folder with prealigned images')
+    parser.add_argument('input_dir', metavar='input_dir',
+                    help='A directory with prealigned images')
     args = parser.parse_args()
-    main(args.inputFolder)
+    dirs = create_dirs()
+
+    # run the procedure
+    # multiple of 2 is best for 'iterations'
+    nzp_gs_smooth.run(args.input_dir, dirs['working_dir'], dirs['out_images'], dirs['out_transforms'], 2, 20)
+
     now = datetime.now()
     current_time_end = now.strftime("%H:%M:%S")
     print("Start time = ", current_time_start)
