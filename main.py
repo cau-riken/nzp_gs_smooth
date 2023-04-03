41e0721769e81cf8fef81a465c2f261651926066
Alexander Woodward <alexander.woodward@riken.jp>
2023-04-03 17:28:49 +0900

    Cleaned code.

--- main.py
+++ main.py
@@ -18,51 +18,59 @@ from datetime import datetime
 import argparse
 import nzp_gs_smooth
 
-def create_dirs(): 
+
+def create_dirs():
     print("Creating required folders.")
     print("This removes existing folders and data. Be careful!")
     cwd = os.getcwd()
-    
+
     working_dir = cwd + "/working/"
-    out_images = cwd+'/out_images'
-    out_transforms = cwd+'/out_transforms'
+    out_images = cwd + '/out_images'
+    out_transforms = cwd + '/out_transforms'
     if os.path.exists(working_dir) is True:
         shutil.rmtree(working_dir)
     if os.path.exists(out_images) is True:
         shutil.rmtree(out_images)
     if os.path.exists(out_transforms) is True:
         shutil.rmtree(out_transforms)
-    os.mkdir(out_images)#Final output images
-    os.mkdir(out_transforms)#Final output transforms
+    os.mkdir(out_images)  # Final output images
+    os.mkdir(out_transforms)  # Final output transforms
     os.mkdir(working_dir)
-    os.mkdir(working_dir+'/input_with_boundary')
-    os.mkdir(working_dir+'/current_iter')
-    os.mkdir(working_dir+'/prev_iter')
-    os.mkdir(working_dir+'/registration_output_transform')
-    os.mkdir(working_dir+'/current_transforms')
-    os.mkdir(working_dir+'/i_hat_image')
-    os.mkdir(working_dir+'/registration_output_image')
+    os.mkdir(working_dir + '/input_with_boundary')
+    os.mkdir(working_dir + '/current_iter')
+    os.mkdir(working_dir + '/prev_iter')
+    os.mkdir(working_dir + '/registration_output_transform')
+    os.mkdir(working_dir + '/current_transforms')
+    os.mkdir(working_dir + '/i_hat_image')
+    os.mkdir(working_dir + '/registration_output_image')
 
-    dirs = {
+    gs_dirs = {
         'working_dir': working_dir,
         'out_images': out_images,
         'out_transforms': out_transforms
     }
-    return dirs
-    
+    return gs_dirs
+
+
 if __name__ == '__main__':
     start_time = datetime.now()
-    
-    parser = argparse.ArgumentParser(description='Standalone version of gauss-seidel smoothing of image stack.')
+
+    parser = argparse.ArgumentParser(
+        description='Standalone version of gauss-seidel smoothing of image stack.')
     parser.add_argument('input_dir', metavar='input_dir',
-                    help='A directory with prealigned images')
+                        help='A directory with prealigned images')
     args = parser.parse_args()
     dirs = create_dirs()
 
     # Run the procedure
-    nzp_gs_smooth.run(args.input_dir, dirs['working_dir'], dirs['out_images'], dirs['out_transforms'], 2, 20)
+    nzp_gs_smooth.run(
+        args.input_dir,
+        dirs['working_dir'],
+        dirs['out_images'],
+        dirs['out_transforms'],
+        2,
+        20)
 
     end_time = datetime.now()
-    
-    print("Total running time = ", end_time-start_time)
-    
+
+    print("Total running time = ", end_time - start_time)
--- nzp_gs_smooth.py
+++ nzp_gs_smooth.py
@@ -9,12 +9,14 @@ Email: alexander.woodward@riken.jp
 
 """
 
+
 def run(in_dir, cur_dir, out_dir_images, out_dir_transforms,
         iterations, ants_thread_count):
     """Apply Gauss-Seidel Iteration Scheme algorithm to a folder of images in sequence.
 
     Args:
-        in_dir: The folder of images to process. Images should be padded with leading zeroes and a starting index of 1, e.g. slice_001.tif
+        in_dir: The folder of images to process. Images should be padded with leading zeroes
+        and a starting index of 1, e.g. slice_001.tif
         cur_dir: The current directory to work from.
         out_dir_images: The directory to place the transformed images.
         out_dir_transforms: Directory for calculated transforms.
@@ -28,11 +30,15 @@ def run(in_dir, cur_dir, out_dir_images, out_dir_transforms,
     import subprocess
     import SimpleITK as sitk
     import copy
-    import os
     from nipype.interfaces.ants import Registration
     from natsort import natsorted
 
-    def reg_run(fixed_image, moving_image, output_transform_prefix, output_warped_image, ants_thread_count):
+    def reg_run(
+            fixed_image,
+            moving_image,
+            output_transform_prefix,
+            output_warped_image,
+            ants_thread_count):
         # os.environ['PATH']+=':/path_to_antsbin'
         reg = Registration()
         reg.inputs.fixed_image = fixed_image
@@ -59,28 +65,29 @@ def run(in_dir, cur_dir, out_dir_images, out_dir_transforms,
         reg.inputs.use_histogram_matching = [True]  # This is the default
         reg.inputs.output_warped_image = output_warped_image
         reg1 = copy.deepcopy(reg)
-        reg1.cmdline
+        # reg1.cmdline
         reg1.run()
 
-    # Be careful to scale by the pixel size when using both SimpleITK and OpenCV functions
+    # Be careful to scale by the pixel size when using both SimpleITK and
+    # OpenCV functions
     def deform_image(img_in, deform_in, scale_factor, out_filename):
         spacing = img_in.GetSpacing()
-        a = sitk.GetArrayFromImage(img_in)
-        a = a.astype(np.float32)
-        c = sitk.GetArrayFromImage(deform_in)
-        mx = np.zeros([deform_in.GetHeight(), deform_in.GetWidth()])
-        my = np.zeros([deform_in.GetHeight(), deform_in.GetWidth()])
-        my = my.astype(np.float32)
-        mx = mx.astype(np.float32)
+        img_a = sitk.GetArrayFromImage(img_in)
+        img_a = img_a.astype(np.float32)
+        img_c = sitk.GetArrayFromImage(deform_in)
+        m_x = np.zeros([deform_in.GetHeight(), deform_in.GetWidth()])
+        m_y = np.zeros([deform_in.GetHeight(), deform_in.GetWidth()])
+        m_y = m_y.astype(np.float32)
+        m_x = m_x.astype(np.float32)
         for j in range(0, deform_in.GetHeight()):
             for i in range(0, deform_in.GetWidth()):
-                pix = c[j, i]
-                mx[j, i] = float(j)+scale_factor * \
-                    float(pix[1]) * (1.0/spacing[1])
-                my[j, i] = float(i)+scale_factor * \
-                    float(pix[0]) * (1.0/spacing[0])
+                pix = img_c[j, i]
+                m_x[j, i] = float(j) + scale_factor * \
+                           float(pix[1]) * (1.0 / spacing[1])
+                m_y[j, i] = float(i) + scale_factor * \
+                           float(pix[0]) * (1.0 / spacing[0])
 
-        out = cv2.remap(a, my, mx, cv2.INTER_CUBIC)
+        out = cv2.remap(img_a, m_y, m_x, cv2.INTER_CUBIC)
         out = sitk.GetImageFromArray(out)
         out.CopyInformation(img_in)
         sitk.WriteImage(out, out_filename)
@@ -88,67 +95,126 @@ def run(in_dir, cur_dir, out_dir_images, out_dir_transforms,
 
     def setup_folders(in_dir):
         # Copy from the input directory first
-        names_in = subprocess.check_output(["ls "+in_dir], shell=True, text=True)
+        names_in = subprocess.check_output(
+            ["ls " + in_dir], shell=True, text=True)
         names_in = names_in.split()
         # Sort the strings using natural sort
         names_in = natsorted(names_in)
         img_count = len(names_in)
-        
+
         # Copy and duplicate first and last (Neumann boundary conditions)
         for i in range(0, img_count):
-            img = sitk.ReadImage(in_dir+names_in[i])
-            index = i+1
+            img = sitk.ReadImage(in_dir + names_in[i])
+            index = i + 1
             sitk.WriteImage(
-                img, cur_dir+"/input_with_boundary/slice_"+format(index, '04d')+".nii")
-            if (index == 1):
+                img,
+                cur_dir +
+                "/input_with_boundary/slice_" +
+                format(
+                    index,
+                    '04d') +
+                ".nii")
+            if index == 1:
                 sitk.WriteImage(
-                    img, cur_dir+"/input_with_boundary/slice_0000.nii")
-            elif (index == img_count):
+                    img, cur_dir + "/input_with_boundary/slice_0000.nii")
+            elif index == img_count:
                 sitk.WriteImage(
-                    img, cur_dir+"/input_with_boundary/slice_"+format(index+1, '04d')+".nii")
+                    img,
+                    cur_dir +
+                    "/input_with_boundary/slice_" +
+                    format(
+                        index +
+                        1,
+                        '04d') +
+                    ".nii")
         # Copy files into directories
-        subprocess.call("cp "+cur_dir+"/input_with_boundary/* "+cur_dir+"/current_iter/", shell=True)
+        subprocess.call("cp " + cur_dir + "/input_with_boundary/* " +
+                        cur_dir + "/current_iter/", shell=True)
         print("Copied files to ./current_iter")
-        subprocess.call("cp "+cur_dir+"/input_with_boundary/* "+cur_dir+"/prev_iter/", shell=True)
+        subprocess.call("cp " + cur_dir + "/input_with_boundary/* " +
+                        cur_dir + "/prev_iter/", shell=True)
         print("Copied files to ./prev_iter")
-        names_in = subprocess.check_output(["ls "+cur_dir+"/current_iter/"], shell=True, text=True)
+        names_in = subprocess.check_output(
+            ["ls " + cur_dir + "/current_iter/"], shell=True, text=True)
         names_in = names_in.split()
 
         return names_in
 
-    def one_pass(start_index,end_index,step,cur_dir,images_names,img_count,ants_thread_count):
-        for j in range(start_index, end_index,step):
+    def one_pass(
+            start_index,
+            end_index,
+            step,
+            cur_dir,
+            current_iteration,
+            images_names,
+            img_count,
+            ants_thread_count):
+        for j in range(start_index, end_index, step):
             # Calculate transform between j-1 and j+1
             # fixed is j-1 moving is j+1
-            reg_run(cur_dir+'/current_iter/'+image_names[j-1], cur_dir+'/current_iter/'+image_names[j+1],
-                    cur_dir+'/registration_output_transform/u_', cur_dir+'/registration_output_image/output_warped_image.nii', ants_thread_count)
-            img_u = sitk.ReadImage(cur_dir+'/registration_output_transform/u_0Warp.nii.gz', sitk.sitkVectorFloat64)
+            reg_run(cur_dir +
+                    '/current_iter/' +
+                    image_names[j - 1], cur_dir +
+                    '/current_iter/' +
+                    image_names[j + 1], cur_dir +
+                    '/registration_output_transform/u_', cur_dir +
+                    '/registration_output_image/output_warped_image.nii', ants_thread_count)
+            img_u = sitk.ReadImage(
+                cur_dir +
+                '/registration_output_transform/u_0Warp.nii.gz',
+                sitk.sitkVectorFloat64)
             # Multiply it by half the deformation field
-            img_JM1 = sitk.ReadImage(cur_dir+'/current_iter/'+image_names[j+step])
+            img_jm1 = sitk.ReadImage(
+                cur_dir + '/current_iter/' + image_names[j + step])
             # Make sure to account for pixel scale factor
-            deform_image(img_JM1, img_u, 0.5, cur_dir+'/i_hat_image/output.nii')
+            deform_image(img_jm1, img_u, 0.5, cur_dir +
+                         '/i_hat_image/output.nii')
             # Register Ij to IHat
-            reg_run(cur_dir+'/i_hat_image/output.nii', cur_dir+'/prev_iter/'+image_names[j],cur_dir+'/registration_output_transform/u_', cur_dir+'/registration_output_image/output_warped_image.nii', ants_thread_count)                    
+            reg_run(
+                cur_dir +
+                '/i_hat_image/output.nii',
+                cur_dir +
+                '/prev_iter/' +
+                image_names[j],
+                cur_dir +
+                '/registration_output_transform/u_',
+                cur_dir +
+                '/registration_output_image/output_warped_image.nii',
+                ants_thread_count)
             # Merge it with previous
-            img_uAccNew = sitk.ReadImage(cur_dir+'/registration_output_transform/u_0Warp.nii.gz', sitk.sitkVectorFloat64)
-            if (t == 0):
-                sitk.WriteImage(img_uAccNew, cur_dir+'/current_transforms/u'+str(j)+'_Warp.nii')
+            img_u_acc_new = sitk.ReadImage(
+                cur_dir + '/registration_output_transform/u_0Warp.nii.gz',
+                sitk.sitkVectorFloat64)
+            # if t == 0:
+            if current_iteration == 0:
+                sitk.WriteImage(img_u_acc_new, cur_dir +
+                                '/current_transforms/u' + str(j) + '_Warp.nii')
             else:
-                img_uAcc = sitk.ReadImage(cur_dir+'/current_transforms/u'+str(j)+'_Warp.nii', sitk.sitkVectorFloat64)
-                img_uAccNew = img_uAcc+img_uAccNew
-                sitk.WriteImage(img_uAccNew, cur_dir+'/current_transforms/u'+str(j)+'_Warp.nii')
+                img_u_acc = sitk.ReadImage(
+                    cur_dir +
+                    '/current_transforms/u' +
+                    str(j) +
+                    '_Warp.nii',
+                    sitk.sitkVectorFloat64)
+                img_u_acc_new = img_u_acc + img_u_acc_new
+                sitk.WriteImage(img_u_acc_new, cur_dir +
+                                '/current_transforms/u' + str(j) + '_Warp.nii')
             # Update Ij using Ij0
-            img_JOrig = sitk.ReadImage(cur_dir+'/input_with_boundary/'+image_names[j])
-            deform_image(img_JOrig, img_uAccNew, 1.0, cur_dir+'/current_iter/'+image_names[j])
+            img_j_orig = sitk.ReadImage(
+                cur_dir + '/input_with_boundary/' + image_names[j])
+            deform_image(img_j_orig, img_u_acc_new, 1.0, cur_dir +
+                         '/current_iter/' + image_names[j])
         # Update boundaries
-        img_b = sitk.ReadImage(cur_dir+'/current_iter/'+image_names[1])
-        sitk.WriteImage(img_b, cur_dir+'/current_iter/'+image_names[0])
-        img_b = sitk.ReadImage(cur_dir+'/current_iter/'+image_names[img_count-2])
-        sitk.WriteImage(img_b, cur_dir+'/current_iter/'+image_names[img_count-1])
+        img_b = sitk.ReadImage(cur_dir + '/current_iter/' + image_names[1])
+        sitk.WriteImage(img_b, cur_dir + '/current_iter/' + image_names[0])
+        img_b = sitk.ReadImage(
+            cur_dir + '/current_iter/' + image_names[img_count - 2])
+        sitk.WriteImage(img_b, cur_dir + '/current_iter/' +
+                        image_names[img_count - 1])
         # Copy current_iter to prev_iter
         for j in range(0, img_count):
-            img_b = sitk.ReadImage(cur_dir+'/current_iter/'+image_names[j])
-            sitk.WriteImage(img_b, cur_dir+'/prev_iter/'+image_names[j])
+            img_b = sitk.ReadImage(cur_dir + '/current_iter/' + image_names[j])
+            sitk.WriteImage(img_b, cur_dir + '/prev_iter/' + image_names[j])
 
     names_in = setup_folders(in_dir)
     img_count = len(names_in)
@@ -157,33 +223,47 @@ def run(in_dir, cur_dir, out_dir_images, out_dir_transforms,
         image_names.append(names_in[i])
 
     # Do nothing if iterations == 0
-    if (iterations > 0):
+    if iterations > 0:
         # iterations = 4
-        #Z = img_count
-        for t in range(0, iterations):
-            print("Starting iteration "+str(t+1)+" of "+str(iterations))
-            if (t % 2 == 0):
-                one_pass(1,img_count-1,1,cur_dir,image_names,img_count,ants_thread_count)
+        # Z = img_count
+        for i in range(0, iterations):
+            print("Starting iteration " + str(i + 1) + " of " + str(iterations))
+            if i % 2 == 0:
+                one_pass(1, img_count - 1, 1, cur_dir, i, image_names,
+                         img_count, ants_thread_count)
             else:
-                one_pass(img_count-2,0,1,cur_dir,image_names,img_count,ants_thread_count)
+                one_pass(img_count - 2, 0, 1, cur_dir, i, image_names,
+                         img_count, ants_thread_count)
 
     # Copy results to output directories
-    subprocess.call("cp "+cur_dir+"/current_iter/* "+out_dir_images, shell=True)
+    subprocess.call("cp " + cur_dir + "/current_iter/* " +
+                    out_dir_images, shell=True)
     # Remove the first and last since they were boundary conditions
-    subprocess.call("rm "+out_dir_images+"/slice_0000.nii", shell=True)
-    subprocess.call("rm "+out_dir_images+"/slice_"+format(img_count-1, '04d')+".nii", shell=True)
+    subprocess.call("rm " + out_dir_images + "/slice_0000.nii", shell=True)
+    subprocess.call("rm " + out_dir_images + "/slice_" +
+                    format(img_count - 1, '04d') + ".nii", shell=True)
     # Copy the transforms to the correct output folder
-    subprocess.call("cp "+cur_dir+"/current_transforms/* "+out_dir_transforms, shell=True)
+    subprocess.call("cp " + cur_dir + "/current_transforms/* " +
+                    out_dir_transforms, shell=True)
     # Copy from the input directory first
-    names_in = subprocess.check_output(["ls "+out_dir_images], shell=True, text=True)
+    names_in = subprocess.check_output(
+        ["ls " + out_dir_images], shell=True, text=True)
     names_in = names_in.split()
     img_count = len(names_in)
-    
+
     # These images should be ordered based on the file naming convention
     for i in range(0, img_count):
-        img = sitk.ReadImage(out_dir_images+"/"+names_in[i])
-        sitk.WriteImage(img, out_dir_images+"/out_"+format(i+1, '04d')+".nii")
+        img = sitk.ReadImage(out_dir_images + "/" + names_in[i])
+        sitk.WriteImage(
+            img,
+            out_dir_images +
+            "/out_" +
+            format(
+                i +
+                1,
+                '04d') +
+            ".nii")
     # Finally remove the old images
-    subprocess.call("rm "+out_dir_images+"/slice_*", shell=True)
+    subprocess.call("rm " + out_dir_images + "/slice_*", shell=True)
 
     return out_dir_images
